76eab575de88d6eafb3907131ef512989351a2b4
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2020-03-06 10:00:02 +0100

    Add files via upload

--- base de datos/inserts.sql
+++ base de datos/inserts.sql
@@ -1,5 +1,5 @@
 	#tipos_categorias
-INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (1, "Acción");
+INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (1, 'AcciÃ³n');
 INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (2, 'Aventuras');
 INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (3, 'Comedia');
 INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (4, 'Terror');
@@ -8,11 +8,11 @@ INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  
 INSERT INTO `tipos_categorias` (`id_categoria`,`descripcion_categoria`) VALUES  (7, 'Drama');
 
 	#tipos_restricciones
-INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (1, 'Apta para todos los públicos','ninguna');
-INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (2, 'No recomendada para menores de 7 años','7');
-INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (3, 'No recomendada para menores de 12 años','12');
-INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (4, 'No recomendada para menores de 16 años','16');
-INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (5, 'No recomendada para menores de 18 años','18');
+INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (1, 'Apta para todos los pÃºblicos','ninguna');
+INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (2, 'No recomendada para menores de 7 aÃ±os','7');
+INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (3, 'No recomendada para menores de 12 aÃ±os','12');
+INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (4, 'No recomendada para menores de 16 aÃ±os','16');
+INSERT INTO `tipos_restricciones` (`id_restriccion`,`descripcion_restriccion`,`edad_restriccion`) VALUES  (5, 'No recomendada para menores de 18 aÃ±os','18');
 	
 	#salas
 INSERT INTO `salas` (`id_sala`,`nombre_sala`, `capacidad_sala`) VALUES  (1, 'Sala 1', '50');
@@ -25,10 +25,10 @@ INSERT INTO `salas` (`id_sala`,`nombre_sala`, `capacidad_sala`) VALUES  (7, 'Sal
 INSERT INTO `salas` (`id_sala`,`nombre_sala`, `capacidad_sala`) VALUES  (8, 'Sala 8', '300');
 
 	#peliculas
-INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (1, 'Aves de presa y la fantabulosa emancipación de Harley Quinn', 'Cathy Yan', '2020-07-02', 3, 5);
-INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (2, 'ADÚ', 'Salvador Calvo', '2020-31-1', 7, 5);
-INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (3, 'Especiales', 'Olivier Nakache, Éric Toledano', '2020-28-02', 3, 5);
-INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (4, 'El Señor de los Anillos: el retorno del Rey', 'Peter Jackson', '2003-17-12', 2, 3);
+INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (1, 'Aves de presa y la fantabulosa emancipaciÃ³n de Harley Quinn', 'Cathy Yan', '2020-07-02', 3, 5);
+INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (2, 'ADÃš', 'Salvador Calvo', '2020-31-1', 7, 5);
+INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (3, 'Especiales', 'Olivier Nakache, Ã‰ic Toledano', '2020-28-02', 3, 5);
+INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (4, 'El SeÃ±or de los Anillos: el retorno del Rey', 'Peter Jackson', '2003-17-12', 2, 3);
 INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (5, 'Kill Bill: Volumen 1', 'Quentin Tarantino', '2003-10-10', 1, 5);
 INSERT INTO `peliculas` (`id_peli`,`nombre_peli`, `nombre_director_peli`, `fecha_peli`, `categoria1_peli`, `restriccion_peli`) VALUES  (6, 'Star Wars: Episodio IX - El ascenso de Skywalker', 'Jeffrey Jacob Abrams', '2019-12-16', 2, 3);
 
@@ -86,10 +86,4 @@ INSERT INTO `valoraciones` (`idusuario_val`, `pelicula_val`, `valoracion_val`) V
 INSERT INTO `valoraciones` (`idusuario_val`, `pelicula_val`, `valoracion_val`) VALUES  (3, 2 , 1);
 INSERT INTO `valoraciones` (`idusuario_val`, `pelicula_val`, `valoracion_val`) VALUES  (3, 6 , 2);
 INSERT INTO `valoraciones` (`idusuario_val`, `pelicula_val`, `valoracion_val`) VALUES  (3, 1 , 4);
-
-
-
-
-
-
 
