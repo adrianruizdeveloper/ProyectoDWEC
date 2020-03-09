419dfc52b8c930a34d8136cb71097525be7791bf
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2020-03-10 00:19:46 +0100

    Add files via upload

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -4,32 +4,29 @@ $('#enviar_registro').click(function () {
     var nombre_registro = $('#nombre_registro').val();
     var apellidos_registro = $('#apellidos_registro').val();
     var fecha_nac_registro = $('#fecha_nac_registro').val();
-    var num_telf_registro = $('#num_telef_registro').val();
+    var num_telef_registro = $('#num_telef_registro').val();
     var correo_registro = $('#correo_registro').val();
     var contrasena_registro = $('#myPassword').val();
     var contrasena_registro2 = $('#contrasena_registro2').val();
-    var futbol_registro = $('#futbol_registro').val();
-    var futbol_sala_registro = $('#futbol_sala_registro').val();
-    var baloncesto_registro = $('#baloncesto_registro').val();
-    var balonmano_registro = $('#balonmano_registro').val();
     $.ajax({
         url: "assets/gets/getregistro.php",
         type: "POST",
         data: {usuario_registro: usuario_registro, nombre_registro: nombre_registro,
         apellidos_registro: apellidos_registro, fecha_nac_registro: fecha_nac_registro,
-        num_telf_registro: num_telf_registro, correo_registro: correo_registro,
-        contrasena_registro: contrasena_registro,contrasena_registro2: contrasena_registro2,
-        futbol_registro: futbol_registro, futbol_sala_registro: futbol_sala_registro,
-        baloncesto_registro: baloncesto_registro, balonmano_registro: balonmano_registro},
+        num_telef_registro: num_telef_registro, correo_registro: correo_registro,
+        contrasena_registro: contrasena_registro,contrasena_registro2: contrasena_registro2},
         success: function (data){
             alert(data);
+            /*
             if (data == 'OK') {
                 $('#myModalregistro').hide();
-                //location.reload();
+                console.log(data);
+                alert(data);
+                
             } else {
-                //alert(data);
-                document.getElementsByClassName("mensaje").text(data);
-            }
+                alert(data);
+                document.getElementsByClassName("error").text("Error al registrar");
+            }*/
         }
     });
 });
