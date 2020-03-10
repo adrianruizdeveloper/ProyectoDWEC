56e52f562bd71e0212308dd4d20c95445c38ce72
ernesstocm <45105313+ernesstocm@users.noreply.github.com>
2020-03-10 21:09:26 +0100

    Add files via upload

--- functions/querys.php
+++ functions/querys.php
@@ -5,11 +5,17 @@ function get_sesions_of_pelis($db, $id_peli)
     $query = 'SELECT * FROM sesiones where peliculas_id_peli = ' . $id_peli . ' and fecha_sesion = \'' . $dia_actual . '\'';
     $request = $db->query($query);
     // print $query;
-    print "<h5 class='title text-center'>" . $dia_actual . "</h5><br>";
     while ($fila = $request->fetch(PDO::FETCH_ASSOC)) {
 
         $hour_format = $date = strtotime($fila['hora_sesion']);
         $hour = date(' H:i', $hour_format);
-        echo "<a href='#' class='btn btn-secondary btn-lg active button-caratula' role='button'aria-pressed='true'>" . $hour . '</a><br>';
+        $hour_now = date(' H:i');
+        $id_button = $hour . "_sesion";
+
+        if ($hour_now <= $hour) {
+            echo "<button id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class='btn btn-secondary btn-lg active button-caratula' role='button'aria-pressed='true'>" . $hour . '</button><br>';
+        } else {
+            echo "<button disabled id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class='btn btn-secondary btn-lg active button-caratula' role='button'aria-pressed='true'>" . $hour . '</button><br>';
+        }
     }
 }
