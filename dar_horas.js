a0886ce981e70a2760d35636f775386406a48a82
ivanojeda <43651811+ivanojeda@users.noreply.github.com>
2020-03-11 13:03:09 +0100

    Add files via upload

---
+++ dar_horas.js
@@ -0,0 +1,23 @@
+$(document).change(function () {
+    $('select[id=fecha]').on('click',function () {
+        
+        var selected = $(this).find('option:selected');
+        var fecha = selected.data('foo'); 
+        var pelicula = $(this).data("id");
+        var id_pelicula = "#"+$(this).val();
+                
+    $.ajax({
+        type: 'POST',
+        url: 'cambio_hora.php',
+        data: {'fecha': fecha, 'pelicula': pelicula},
+    })
+            .done(function(data){
+                $(id_pelicula).html(data)
+            })
+            .fail(function(){
+                alert('Error al cargar las horas')
+            })
+      });
+            
+});
+
