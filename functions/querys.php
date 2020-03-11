87da3b9c4a90822f40a8b2f0ffcc488923578b03
Ernesto <ernesstocm@gmail.com>
2020-03-11 13:01:49 +0100

    index
    index

--- functions/querys.php
+++ functions/querys.php
@@ -5,7 +5,9 @@ function get_sesions_of_pelis($db, $id_peli)
     $query = 'SELECT * FROM sesiones where peliculas_id_peli = ' . $id_peli . ' and fecha_sesion = \'' . $dia_actual . '\'';
     $request = $db->query($query);
     // print $query;
+    echo "<div class='row justify-content-center'>";
     while ($fila = $request->fetch(PDO::FETCH_ASSOC)) {
+        echo "<div class='col-xs-3 col-md-2 col-lg-2 button_div_sesion'>";
 
         $hour_format = $date = strtotime($fila['hora_sesion']);
         $hour = date(' H:i', $hour_format);
@@ -13,9 +15,11 @@ function get_sesions_of_pelis($db, $id_peli)
         $id_button = $hour . "_sesion";
 
         if ($hour_now <= $hour) {
-            echo "<button id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class='btn btn-secondary btn-lg active button-caratula' role='button'aria-pressed='true'>" . $hour . '</button><br>';
+            echo "<button id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class=' button-caratula btn btn-secondary btn-lg active ' role='button'aria-pressed='true'>" . $hour . '</button>';
         } else {
-            echo "<button disabled id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class='btn btn-secondary btn-lg active button-caratula' role='button'aria-pressed='true'>" . $hour . '</button><br>';
+            echo "<button disabled id='" . $id_button . "' data_id_pelicula='" . $fila['peliculas_id_peli'] . "' data_id_sesion=' " . $fila['id_sesion'] . "' class=' button-caratula btn btn-secondary btn-lg active ' role='button'aria-pressed='true'>" . $hour . '</button>';
         }
+        echo "</div>";
     }
+    echo "</div>";
 }
--- index.php
+++ index.php
@@ -11,19 +11,24 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
+    <meta name="viewport" content="width=250, initial-scale=1">
 </head>
 
 <body>
     <?php session_start();
     require "includes/cabecera.php";
     cabecera("index");
+    $dia_actual = date("d-m-Y");
     ?>
 
 
+<div class="row text-center">
     <div class="container body center">
-        <div class="row">
-            <div class="col-md-9">
+        <?php
+        echo "<h1 class='text-center'>Sesiones: " . $dia_actual . "</h1>";
+        
+        ?>
+            <div class="col-md-12">
                 <div class="row">
                     <?php
                     include('functions/connectPDO.php');
@@ -33,20 +38,18 @@
                     $db->exec('use peliculas;');
                     $peliculas = $db->query("SELECT * FROM sesiones INNER JOIN peliculas ON sesiones.peliculas_id_peli = peliculas.id_peli where sesiones.fecha_sesion = '" . $dia_actual . "' group by peliculas_id_peli");
                     foreach ($peliculas as $fila) {
-                        echo "<div class='col-xs-12 col-md-6 col-lg-4 text-center'>";
-                        echo "<h4 class='title'>" . $fila["nombre_peli"] . "</h4><br>";
+                        echo "<div class='col-xs-12 col-md-6 col-lg-3    text-center '>";
+                        echo "<h4 class='title'>" . $fila["nombre_peli"] . "</h4>";
                         echo "<img  class='caratula' src='images/" . $fila["caratula"] . "'><br> ";
-                        echo "</div>";
-                        echo "<div class='col-xs-12 col-md-6 col-lg-8 text-center'>";
                         get_sesions_of_pelis($db, $fila["peliculas_id_peli"]);
                         echo "</div>";
                     }
                     ?>
                 </div>
             </div>
-            <div class="col-md-3 sidebar">
+            <!-- <div class="col-md-3 sidebar">
                 <p>esto es el sidebar</p>
-            </div>
+            </div> -->
         </div>
     </div>
 
